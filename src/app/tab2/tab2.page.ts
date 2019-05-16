826ff44cc923e04032248324ab1b06986cf34745
aureodark <aureofernandes@hotmail.com.br>
2019-05-15 21:38:08 -0300

    second

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
 
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+
+  private posLat: number = 0;
+  private posLog: number = 0;
+  maps: string = null
+  imagem = "https://imagens.canaltech.com.br/236607.471195-Lua.jpg";
+
+  constructor(
+    private geolocation: Geolocation
+  ) { }
+
+  local() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.posLat = resp.coords.latitude
+      this.posLog = resp.coords.longitude
+      this.maps = "https://www.youtube.com/"
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+  }
+
+}
